b9a2f9e5a4ee3ce67d35a87e9c48c204a2bf2d67
Hannah Malm <hannahemalm616@gmail.com>
2021-08-28 20:13:28 -0400

    commit tree f435b153b57893e21e9a2bbacd1932ceb3b21d98

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,8 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+    #need to add this in to set the instance variable called @author
+    @author = @post.author
   end
 
   def new
